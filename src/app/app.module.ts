c8c7a34bf3fc02f53f2f62c7a32dd782ec642066
KarineNascimento <55191012+KarineNascimento@users.noreply.github.com>
2024-03-05 15:04:05 -0300

    Listagem dos produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ListagemUsuariosComponent } from './pages/usuarios/listagem/listagem.component';
 import { CadastroEdicaoUsuariosComponent } from './pages/usuarios/cadastro-edicao/cadastro-edicao.component';
+import { ListagemProdutosComponent } from './pages/produtos/listagem/listagem.component';
+import { CadastroEdicaoProdutosComponent } from './pages/produtos/cadastro-edicao/cadastro-edicao.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageTitleComponent } from './components/page-title/page-title.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,6 +21,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FooterComponent,
     ListagemUsuariosComponent,
     CadastroEdicaoUsuariosComponent,
+    ListagemProdutosComponent,
+    CadastroEdicaoProdutosComponent,
     HomeComponent,
     PageTitleComponent
   ],
---
+++ src/app/interfaces/produto.ts
@@ -0,0 +1,7 @@
+export interface IProduto {
+  id?: number;
+  nome_produto: string;
+  codigo_barras: string;
+  quantidade: number;
+  preco: number;
+}
